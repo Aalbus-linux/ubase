095e26680e87bb5207a590d5918afa2f99235cf7
Jens Staal <staal1978@gmail.com>
2020-12-30 05:24:32 +0100

    custom config.mk

--- config.mk
+++ config.mk
@@ -2,14 +2,13 @@
 VERSION = 0.1
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+PREFIX = /
+MANPREFIX = /usr/share/man
 
-CC = cc
-AR = ar
-RANLIB = ranlib
+CC = clang
+AR = llvm-ar
+RANLIB = llvm-ranlib
 
 CPPFLAGS = -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
-CFLAGS   = -std=c99 -Wall -Wextra
 LDLIBS   = -lcrypt
 LDFLAGS  = -s
